084bff7c0063d7208026b921f547836795d5237a
djmakwana797 <dmakwana979@rku.ac.in>
2020-10-05 12:40:18 +0530

    JSON parsing from URL using volley library

--- app/src/main/java/com/rku/tutorial11/MainActivity.java
+++ app/src/main/java/com/rku/tutorial11/MainActivity.java
@@ -10,8 +10,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     ListView lstData;
     CustomAdapter adapter;
+    RequestQueue requestQueue;
+    JsonArrayRequest jsonArrayRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,54 +50,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        new MyAsyncTask().execute();
+        volleyAPICall();
     }
 
-    class MyAsyncTask extends AsyncTask {
-        ProgressDialog dialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog = new ProgressDialog(MainActivity.this);
-            dialog.setMessage("Loading data");
-            dialog.show();
-        }
-
-        @Override
-        protected Object doInBackground(Object[] objects) {
-            StringBuffer response = new StringBuffer();
-            try {
-                URL url = new URL(Util.URL_USERS);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                InputStreamReader ir = new InputStreamReader(urlConnection.getInputStream());
-                BufferedReader br = new BufferedReader(ir);
-                String inputLine = null;
-
-                while((inputLine = br.readLine())!=null){
-                    response.append(inputLine);
+    private void volleyAPICall() {
+        final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                Util.URL_USERS,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Util.userdata = response;
+                        adapter = new CustomAdapter(MainActivity.this,Util.userdata);
+                        lstData.setAdapter(adapter);
+                        if(dialog.isShowing())dialog.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
                 }
-                br.close();
-                ir.close();
-
-                Util.userdata = new JSONArray(response.toString());
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
-            adapter = new CustomAdapter(MainActivity.this,Util.userdata);
-            lstData.setAdapter(adapter);
-
-            if(dialog.isShowing())dialog.dismiss();
-        }
+        );
+        requestQueue.add(jsonArrayRequest);
+        dialog.setMessage("Loading data");
+        dialog.show();
     }
 }
